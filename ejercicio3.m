9d233910e7f3a77c0da8b263855e8a40783d82be
DanielAcosta45 <50054203+DanielAcosta45@users.noreply.github.com>
2019-04-27 11:38:29 -0500

    Ingresar un numero del 1 al 12 dar mes
    El usuario ingresa un numero del 1 al 12 y te da automáticamente el mes que le corresponde a dicho numero

---
+++ ejercicio3.m
@@ -0,0 +1,36 @@
+clear all
+clc
+close all
+a=input('Digite un numero del 1 al 12 ');
+if a==1
+ disp('Enero');
+elseif a==2
+    disp('Febrero')
+elseif a==3
+    disp('marzo')
+elseif a==4
+    disp('abril')
+elseif a==5
+    disp('Mayo')
+elseif a==6
+    disp('Junio')  
+elseif a==7
+    disp('Julio')
+ elseif a==8
+    disp('Agosto')
+ elseif a==9
+    disp('Septiembre')
+elseif a==10
+    disp('Octubre') 
+elseif a==11
+    disp('Noviembre')
+elseif a==12
+    disp('Diciembre');
+    
+end
+
+                                                     
+                                                        
+
+
+
